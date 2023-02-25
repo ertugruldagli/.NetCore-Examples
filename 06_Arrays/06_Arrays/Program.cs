36178d7073b5abcb3f4e64e6fdccac738f48d5bb
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-02-25 20:35:44 +0300

    hata commit edildi

--- 06_Arrays/06_Arrays/Program.cs
+++ 06_Arrays/06_Arrays/Program.cs
@@ -367,6 +367,50 @@ internal class Program
 
         #endregion
 
+        #region Başka bir örnek
+
+
+        //int bolunecek, bolen;
+
+        //try
+        //{
+        //    Console.Write("-> Bölünecek Değeri Girin: ");
+
+        //    bolunecek = Convert.ToInt32(Console.ReadLine());
+
+        //    Console.Write("-> Bölen Değeri Girin: ");
+
+        //    bolen = Convert.ToInt32(Console.ReadLine());
+
+        //    double sonuc = (bolunecek / bolen);
+
+        //    Console.WriteLine("-> {0} değerinin {1} değerine bölümünün sonucu: {3}", bolunecek, bolen, sonuc);
+        //}
+        //catch (DivideByZeroException e)
+        //{
+        //    Console.WriteLine("\n-> Hata! Sıfıra bölüm işlemi gerçekleştirilemez.\n-> Hata Mesajı: {0}\n", e.Message);
+        //}
+        //catch (FormatException e)
+        //{
+        //    Console.WriteLine("\n-> Hata! Sadece Int32 türünde veri girişi yapabilirsiniz.\n-> Hata Mesajı: {0}\n", e.Message);
+        //}
+        //catch (OverflowException e)
+        //{
+        //    Console.WriteLine("\n-> Hata! Değişgen sınırların aştınız.\n-> Hata Mesajı: {0}\n", e.Message);
+        //}
+        //catch (Exception e) // Geri kalan tüm hata durumlarını kapsar
+        //{
+        //    Console.WriteLine("\n-> Beklenmedik Bir Hata!\n-> Hata Mesajı: {0}\n", e.Message);
+        //}
+        //finally
+        //{
+        //    Console.WriteLine("-> Finally Bloğu Çalışıyor.\n");
+        //}
+
+        #endregion
+
+
+
         Console.ReadKey();
     }
 }

2cf272ca7918ec9204b3bb09386566a935f5ec28
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-02-27 14:51:16 +0300

    c

--- Studys/Methods/Program.cs
+++ Studys/Methods/Program.cs
@@ -3,6 +3,137 @@ using System.Security.Cryptography;
 
 internal class Program
 {
+  
+
+    private static void Main(string[] args)
+    {
+        #region dikdörtgen alan hesabı
+        //int k1, k2,alan;
+
+
+        //Console.WriteLine("kısa kenar giriniz: ");
+        //k1=Convert.ToInt32(Console.ReadLine());
+
+        //Console.WriteLine("uzun kenar giriniz: ");
+        //k2=Convert.ToInt32(Console.ReadLine());
+
+        //alan = k1 * k2;
+
+        //Console.WriteLine("alani: "+alan);
+        #endregion
+
+        #region 1. geriye deger döndürmeyen ve parametre almayan
+
+
+        //Hesapla();
+
+
+        #endregion
+
+        #region 2. geriye deger döndürmeyen ve parametre alan
+
+        //Console.WriteLine("kısa kenar giriniz: ");
+        //int kk = Convert.ToInt32(Console.ReadLine());
+
+        //Console.WriteLine("uzun kenar giriniz: ");
+        //int uk = Convert.ToInt32(Console.ReadLine());
+        //Hesapla(kk,uk);
+
+        //Hesapla(5,6);
+
+
+        #endregion
+
+        #region 3. geriye deger döndüren parametre almayan
+
+        //int sonuc=HesaplaPmsiz(); //sonuc = alan (return'den gelen)
+        //Console.WriteLine("Alan: {0}", sonuc);
+
+        #endregion
+
+        #region 4. geriye deger döndüren parametre alan
+
+        //Console.WriteLine("kısa kenar giriniz: ");
+        //int kk = Convert.ToInt32(Console.ReadLine());
+
+        //Console.WriteLine("uzun kenar giriniz: ");
+        //int uk = Convert.ToInt32(Console.ReadLine());
+
+        //Console.WriteLine("alan: {0}", HesaplaPrmli(kk,uk));
+
+        //HesaplaPrmli(10,30);
+
+        #endregion
+
+        #region workout01
+        //uygun method yapısı kullanarak dört işlem gerçekleştiriniz. Birden fazla method tanımlayabilirsiniz.
+
+        //Console.WriteLine("s1 giriniz: ");
+        //int s1 = Convert.ToInt32(Console.ReadLine());
+
+        //Console.WriteLine("s2 giriniz: ");
+        //int s2 = Convert.ToInt32(Console.ReadLine());
+
+        //Console.WriteLine("sonuc: "+ Topla(s1,s2));
+
+        //Hepsi(s1,s2);
+
+        #endregion
+
+        #region workout01-ek
+        //Console.WriteLine("kısa kenar giriniz: ");
+        //int kk = Convert.ToInt32(Console.ReadLine());
+
+        //Console.WriteLine("uzun kenar giriniz: ");
+        //int uk = Convert.ToInt32(Console.ReadLine());
+
+        //Console.WriteLine("işlem türünü seçiniz:");
+        //string secenek= Console.ReadLine();
+
+        //DortIslem(kk, uk, secenek);
+
+        //Console.WriteLine("işlem türü: "+secenek);
+        //Console.WriteLine("sonuc :{0}", DortIslem(kk, uk, secenek));
+
+        #endregion
+
+        #region methodlarda aşırı yüklenme
+
+        //Console.WriteLine("2li toplama:{0}", toplamaIslemi(45,45));
+        //Console.WriteLine("3li toplama:{0}", toplamaIslemi(45, 45,45));
+        //Console.WriteLine("4li toplama:{0}", toplamaIslemi(45, 45,45,45));
+
+
+
+        #endregion
+
+        #region workout02 - menu
+
+        //ekrana seçenekli bir menü gelecek. seçeneklerim 1- hesap makinesi, 2-tek/çift kontrol.  buna göre methoları hazırlayınız.. menüyü ekrana getirme hesap mak. tekcift kontrol
+
+        Console.WriteLine("kısa kenar giriniz: ");
+        int kk = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("uzun kenar giriniz: ");
+        int uk = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("işlem türünü seçiniz:");
+        string secenek = Console.ReadLine();
+
+         HesapMak(kk, uk, secenek);
+
+
+
+        #endregion
+
+
+
+
+
+        Console.ReadKey();  
+    }
+
+
     #region Methodlar
 
     private static void Hesapla()
@@ -139,124 +270,54 @@ internal class Program
     }
 
 
-
-
-    private static void Main(string[] args)
+    public static void Menu()
     {
-        #region dikdörtgen alan hesabı
-        //int k1, k2,alan;
 
+    }
+    public static void HesapMak(int sayi1, int sayi2, string secenek)
+    {
+        int sonuc = 0;
+        if (secenek == "+")
+        {
+            sonuc = sayi1 + sayi2;
+        }
+        else if (secenek == "-")
+        {
+            sonuc = sayi1 - sayi2;
+        }
+        else if (secenek == "*")
+        {
+            sonuc = sayi1 * sayi2;
+        }
+        else
+        {
+            try
+            {
+                sonuc = sayi1 / sayi2;
+            }
+            catch
+            {
+                Console.WriteLine("sayi2 degeri 0 olamaz!");
 
-        //Console.WriteLine("kısa kenar giriniz: ");
-        //k1=Convert.ToInt32(Console.ReadLine());
-
-        //Console.WriteLine("uzun kenar giriniz: ");
-        //k2=Convert.ToInt32(Console.ReadLine());
-
-        //alan = k1 * k2;
-
-        //Console.WriteLine("alani: "+alan);
-        #endregion
-
-        #region 1. geriye deger döndürmeyen ve parametre almayan
-
-
-        //Hesapla();
-
-
-        #endregion
-
-        #region 2. geriye deger döndürmeyen ve parametre alan
-
-        //Console.WriteLine("kısa kenar giriniz: ");
-        //int kk = Convert.ToInt32(Console.ReadLine());
-
-        //Console.WriteLine("uzun kenar giriniz: ");
-        //int uk = Convert.ToInt32(Console.ReadLine());
-        //Hesapla(kk,uk);
-
-        //Hesapla(5,6);
-
-
-        #endregion
-
-        #region 3. geriye deger döndüren parametre almayan
-
-        //int sonuc=HesaplaPmsiz(); //sonuc = alan (return'den gelen)
-        //Console.WriteLine("Alan: {0}", sonuc);
-
-        #endregion
-
-        #region 4. geriye deger döndüren parametre alan
-
-        //Console.WriteLine("kısa kenar giriniz: ");
-        //int kk = Convert.ToInt32(Console.ReadLine());
-
-        //Console.WriteLine("uzun kenar giriniz: ");
-        //int uk = Convert.ToInt32(Console.ReadLine());
-
-        //Console.WriteLine("alan: {0}", HesaplaPrmli(kk,uk));
-
-        //HesaplaPrmli(10,30);
-
-        #endregion
-
-        #region workout01
-        //uygun method yapısı kullanarak dört işlem gerçekleştiriniz. Birden fazla method tanımlayabilirsiniz.
-
-        //Console.WriteLine("s1 giriniz: ");
-        //int s1 = Convert.ToInt32(Console.ReadLine());
-
-        //Console.WriteLine("s2 giriniz: ");
-        //int s2 = Convert.ToInt32(Console.ReadLine());
-
-        //Console.WriteLine("sonuc: "+ Topla(s1,s2));
-
-        //Hepsi(s1,s2);
-
-        #endregion
-
-        #region workout01-ek
-        //Console.WriteLine("kısa kenar giriniz: ");
-        //int kk = Convert.ToInt32(Console.ReadLine());
-
-        //Console.WriteLine("uzun kenar giriniz: ");
-        //int uk = Convert.ToInt32(Console.ReadLine());
-
-        //Console.WriteLine("işlem türünü seçiniz:");
-        //string secenek= Console.ReadLine();
-
-        //DortIslem(kk, uk, secenek);
-
-        //Console.WriteLine("işlem türü: "+secenek);
-        //Console.WriteLine("sonuc :{0}", DortIslem(kk, uk, secenek));
-
-        #endregion
-
-        #region methodlarda aşırı yüklenme
-
-        //Console.WriteLine("2li toplama:{0}", toplamaIslemi(45,45));
-        //Console.WriteLine("3li toplama:{0}", toplamaIslemi(45, 45,45));
-        //Console.WriteLine("4li toplama:{0}", toplamaIslemi(45, 45,45,45));
-
-
-
-        #endregion
-
-        #region workout02 - menu
-
-        //ekrana seçenekli bir menü gelecek. seçeneklerim 1- hesap makinesi, 2-tek/çift kontrol.  buna göre methoları hazırlayınız.. menüyü ekrana getirme hesap mak. tekcift kontrol
-
-        #endregion
-
-
-
-
-
-        Console.ReadKey();  
+                sonuc = sayi2 / sayi1;
+            }
+        }
+       
     }
 
-  
+    public static string TekCift(int sayi)
+    {
+        string a;
+        if (sayi % 2 == 0)
+        {
+             a = "sayi çiftir.";
+        }
+        else
+        {
+             a = "tektir";
+        }
+        return a ;
+    }
 
 
 }

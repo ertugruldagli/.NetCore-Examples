0f1b80b2cbc9a8713567f3102e4eced99d276005
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-02-27 10:03:03 +0300

    commit

--- WorkOuts/02/Program.cs
+++ WorkOuts/02/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        float vf_monthlySalary = 8500f;
+        int year =1;
+
+        Console.WriteLine("Başlangıç maasi :  {0} PNG Kina'dır...", String.Format("{0:###,###}",vf_monthlySalary));
+
+        for (year = 2; year <= 5; year++)
+        {
+            vf_monthlySalary = vf_monthlySalary * 1.15f;
+            Console.WriteLine("{0}. yıl maasiniz : {1} PNG Kina", year.ToString(),Math.Ceiling(vf_monthlySalary));
+        }
+
+        Console.ReadKey();
+    }
+}

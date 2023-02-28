71a440bbcfa7f6015c7a1d5eceffb99741e42214
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-02-28 09:37:34 +0300

    c

--- Studys/WorkOut-EmeklilikHesaplama/Program.cs
+++ Studys/WorkOut-EmeklilikHesaplama/Program.cs
@@ -2,6 +2,18 @@
 {
     private static void Main(string[] args)
     {
-        
+        //Erkeklerin 25 yıl
+        // kadınalrın 20 yıl da emekli olacağını bir ortamda
+        //klavyeden cinsiyet ve sigorta prim gün sayısı girilecek
+        //emekliliğe ne kadar kaldı?
+
+        // 1 yıl=360
+        //1 ay =30
+
+
+
+
+
+        Console.ReadKey();
     }
 }

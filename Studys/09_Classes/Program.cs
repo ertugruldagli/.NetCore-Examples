c1e4de16c188787289fbcf92cfc4fc0c957dba88
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-03-01 09:41:47 +0300

    c1

--- Studys/09_Classes/Program.cs
+++ Studys/09_Classes/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        //personel kayıt sistemi: clsPersonel; persosnelID, ad,soyadı, tc, maas
+        //Personel kaydı oluşturmak için setPerosnelData isimli method oluşturulacak. 
+        //Personel bilgilerini göstermek için getPersonelData isimli bir method oluşturulacak.
+        //Sınıf tanımlaması bititkten sonra işlemler orada tanımlanacak. 
+
+
+
+
+
+        Console.ReadKey();
+
+    }
+}

ce9cd6aa1aaf46996f3f3ffebb2d43498c0ada15
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-03-01 10:10:46 +0300

    iki obje commit edildi

--- Studys/09_Classes/Program.cs
+++ Studys/09_Classes/Program.cs
@@ -3,7 +3,7 @@
 internal class Program
 {
     private static void Main(string[] args)
-    { 
+    {
         //Personel kayıt sistemi: clsPersonel; persosnelID, ad,soyadı, tc, maas
         //Personel kaydı oluşturmak için setPerosnelData isimli method oluşturulacak. 
         //Personel bilgilerini göstermek için getPersonelData isimli bir method oluşturulacak.
@@ -12,6 +12,34 @@ internal class Program
 
         //Personel Bilgileri 1
 
+        Console.WriteLine("Unitum Cerebris Commuitas Personel Listesi");
+        Console.WriteLine("------------------------------------------");
+
+
+        //nesne oluşturma
+        clsPersonel1 per1= new clsPersonel1();
+
+        // per1.setPersonelData(1,"ertu","hjhksjd","fsdsfsdf","sdfsdf",5600);
+
+        per1.PersonelID = 1;
+        per1.Ad = "Ertu";
+        per1.Soyad = "Dagli";
+        per1.TCKNo = "11951441286";
+        per1.Bolum = "IT";
+        per1.Maas = 1;
+
+        per1.getPersonelData();
+
+
+        clsPersonel1 per2 = new clsPersonel1(); //başka bir nesne oluşturuluyor.
+        per2.PersonelID = 2;
+        per2.Ad = "Uras";
+        per2.Soyad = "Goceri";
+        per2.TCKNo = "45657654351";
+        per2.Bolum = "Post";
+        per2.Maas = 1000;
+
+        per2.getPersonelData();
 
 
         Console.ReadKey();
@@ -46,7 +74,7 @@ internal class Program
         public void getPersonelData()
         {
 
-            Console.WriteLine($"\n\nPersonel No: {PersonelID}\nAdı ve soyadı: {Ad} {Soyad}\n TCKNo: {TCKNo}\nBolum: {Bolum}\nMaas:{Maas} PNG Kina");
+            Console.WriteLine($"\n\nPersonel No: {PersonelID}\nAdı ve soyadı: {Ad} {Soyad}\nTCKNo: {TCKNo}\nBolum: {Bolum}\nMaas:{Maas} PNG Kina");
             
         }
 

41db1d9b90574da3222e0ab4512b177ef3c25d0a
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-03-01 13:33:20 +0300

    c

--- Studys/09_Classes/Program.cs
+++ Studys/09_Classes/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Cryptography;
+﻿using System;
+using System.Security.Cryptography;
 
 internal class Program
 {
@@ -65,18 +66,48 @@ internal class Program
         #region Öğrenci örneği
         //Bir class oluşturulacak; clsOgrenci: ogrNO,ogrAD,ogrSoyad, ders ve dersntu
         //2 tane method olacak 1 tanesi ogrencinin verilerini tutacak, diğeri ise ogrencinin aldığı nota göre 50 kontrol degeri ile geçti kaldı çıktısı verilsin.
-
-        clsOgrenci o1 =new clsOgrenci();
-       
-        o1.OgrenciNo = 0;
-        o1.Ad = "ertugrul";
-        o1.Soyad = "dagli";
-        o1.Ders = "mat";
-        o1.Not = 56;
-
-        o1.getOgrenci();
-        o1.NotKontrol(o1.Not);
         
+        //clsOgrenci o1 =new clsOgrenci();
+       
+        //o1.OgrenciNo = 0;
+        //o1.Ad = "ertugrul";
+        //o1.Soyad = "dagli";
+        //o1.Ders = "mat";
+        //o1.Not = 56;
+
+        //o1.getOgrenci();
+        //o1.NotKontrol(o1.Not);
+
+
+
+        #endregion
+
+        #region personel örneği 2
+
+        //bu örneğin amacı class yapısının bu source dosyasından farklı bir yerde tanımlanması ve kullanılması üzerine
+        //bir şirket var
+        //5 elemanı var ..  ad soyad ünvan maas ve izingunu
+        //bir tane method olacak 
+        //method içerisinde öncelikle  ad soyad bilgsini dolu olduğu durum da 
+        //maasının miktarına göre o personelin kaç gün izin hakkı olacağı belirlencek
+        //maas 2500 kina altındaysa düz personel izin günü 14
+        //mass 10000-14000 arasındaysa kdemli personel ve izin günü 20 olacak
+        //maas 14001-15500 arasındaysa kıdemli+ personel //izin günü 24 olacak olaca
+        // maas 15500 üzeindeyse uzman personel olarak çıkış veren şekilde olacak izin günü 30 olacak
+        //not: personelin maas bilgisinin belirlenmesi bilgisayara bırakılacak... random.. aralık 1000-20000 aralığı olsun.
+        //
+        
+        clsPersonel2 per2=new clsPersonel2();
+        per2.PersonelAd = "ERtugrul";
+        per2.PersonelSoyad = "dagli";
+        per2.Unvan = "Kıdemli Uzman";
+
+        Random rnd = new Random();
+        per2.Maas = rnd.Next(1000, 20000);
+
+
+        per2.getPersonel();
+        per2.PersonelMaas(per2.Maas);
 
 
         #endregion
@@ -119,9 +150,7 @@ internal class Program
 
 
     }
-    #endregion
-
-     class clsOgrenci
+    class clsOgrenci
     {
         public int OgrenciNo;
         public string Ad;
@@ -129,7 +158,7 @@ internal class Program
         public string Ders;  // işlem yapılmayacağı(*+-/) için bellekten kazanç sağlamak adına string veri tipinde değer döndürür.
         public int Not;
 
-       
+
 
         public void getOgrenci()
         {
@@ -139,7 +168,7 @@ internal class Program
 
         public void NotKontrol(int Not)
         {
-        
+
             if (Not >= 50)
             {
 
@@ -150,9 +179,52 @@ internal class Program
                 Console.WriteLine("kaldı");
             }
         }
-           
 
-     }
+
+    }
+    #endregion
+
+    class clsPersonel2
+    {
+        public string PersonelAd;
+        public string PersonelSoyad;
+        public string Unvan;
+        public int Maas;
+        public int IzınGunu=14;
+
+        public void getPersonel()
+        {
+
+            Console.WriteLine($"Adı ve soyadı: {PersonelAd} {PersonelSoyad}\nUnvan: {Unvan}\nMaas: {Maas}");
+        }
+
+        public void PersonelMaas(int Maas)
+        {
+
+          
+
+            if (Maas>=2500 && Maas<=10000)
+            {
+                Console.WriteLine("Düz Personel  izin gün sayısı: " + IzınGunu);
+            }
+            else if (Maas>=10001 && Maas<=14000)
+            {
+                Console.WriteLine("Kıdemli Perosnel  izin gün sayısı : 20");
+            }
+            else if (Maas >= 14001 && Maas <= 15000)
+            {
+                Console.WriteLine("Kıdemli Perosnel+  izin gün sayısı : 24");
+            }else if (Maas>15500)
+            {
+                Console.WriteLine("Uzman izin gün sayısı : 30");
+            }
+           
+        }
+    }
+
+
+  
+
 
 }
 

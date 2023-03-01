a5d22bf91784f190421a68a7c5a89c6bc11bdea5
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-03-01 14:10:48 +0300

    crate the getPersonelinfo

--- Studys/09_Classes/clsPersonel2.cs
+++ Studys/09_Classes/clsPersonel2.cs
@@ -23,38 +23,62 @@ namespace _09_Classes
 
             if (pAd.Length>0 && pSoyad.Length>0) // ad ve soyadın tam girilmesi kontrol ediliyor.
             {
-                this.Ad = pAd;
-                this.Soyad = pSoyad;
-                this.Maas = pMaas;
-                this.IzınGunu = 14;
 
-                if (pMaas>=10000 && pMaas<=14000)
+                if (pMaas >= 2500)
                 {
-                    this.Unvan = "Kıdemli Personel";
-                    this.IzınGunu = 20;
-                }
-                    else if(pMaas >= 14001 && pMaas <= 15500)
+
+                    this.Ad = pAd;
+                    this.Soyad = pSoyad;
+                    this.Maas = pMaas;
+                    this.IzınGunu = 14;
+
+
+
+                    if (pMaas >= 10000 && pMaas <= 14000)
+                    {
+                        this.Unvan = "Kıdemli Personel";
+                        this.IzınGunu = 20;
+                    }
+                    else if (pMaas >= 14001 && pMaas <= 15500)
                     {
                         this.Unvan = "Kıdemli+ Personel";
                         this.IzınGunu = 24;
-                    }else if (pMaas>15500)
+                    }
+                    else if (pMaas > 15500)
                     {
                         this.Unvan = "Uzman Personel";
                         this.IzınGunu = 30;
                     }
+                    else
+                    {
+                        this.Unvan = "Jr. Personel";
+
+                    }
+                }
                 else
                 {
-                    this.Unvan = "Jr Personel";
-                     
+                    Console.WriteLine("Personel oluşturulamadı!");
                 }
 
             }
+            else
+            {
+                Console.WriteLine("İsim Bilgisi hatalı girilmiştir.");
+            }
 
 
+        }
 
-
-
-
+        public  string getPersonelBilgileri()
+        {
+            if (Unvan!=null)
+            {
+                return $"Personel Ad: {Ad}\n Soyadı: {Soyad}\n Unvan: {Unvan}\n Maasi: {Maas} PNG Kina\n IzınGunu: {IzınGunu} gün";
+            }
+            else
+            {
+                return "Lütfen Personeli Oluşturunuz.";
+            }
         }
         
 

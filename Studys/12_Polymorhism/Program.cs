f8c544b6700bbb000c430e06f8fa084d79dc2c0f
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-03-02 14:16:03 +0300

    polimorhism

---
+++ Studys/12_Polymorhism/Program.cs
@@ -0,0 +1,18 @@
+﻿using _;
+using _12_Polymorhism;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+       clsTutorials tutorials = new clsTutorials();
+
+        tutorials.setTutorial(1,"C#"); //1. formunu kullanıyorum. 2 parametreli olan
+        Console.WriteLine(tutorials.getTutorialName());
+
+        tutorials.setTutorial("java"); //2. formunu kullnıyoruz tek parametreli olan
+
+        Console.WriteLine();
+    }
+}
---
+++ Studys/12_Polymorhism/clsTutorials.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_Polymorhism
+{
+    internal class clsTutorials
+    {
+
+       protected int TutorialID;
+       protected string TutorialName;
+
+        public void setTutorial(int pID, string pTutorialName)
+        {
+            this.TutorialID= pID;
+            this.TutorialName= pTutorialName;
+        }
+        public void setTutorial( string pTutorialName)
+        {
+            
+            this.TutorialName = pTutorialName;
+        }
+
+        public  string getTutorialName()
+        {
+            return this.TutorialName;
+        }
+
+    }
+}

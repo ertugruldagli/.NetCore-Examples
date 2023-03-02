c367b1bab9da5ba924d61b927f28d3a0e88d1052
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-03-02 15:07:21 +0300

    the son

--- Studys/13_Constructor/Program.cs
+++ Studys/13_Constructor/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using _13_Constructor;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+
+        clsTest test= new clsTest();
+
+        Console.WriteLine($"i degeri {test.i} - s degeri {test.s}- b degeri{test.b}");
+
+
+        Console.ReadKey();
+    }
+}
---
+++ Studys/13_Constructor/clsTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_Constructor
+{
+    internal class clsTest
+    { //ctor kısa ismi 
+
+        public int i; //default: 0
+        public string s; //defalt: ""
+        public bool b; //defalt: false
+
+        //class ismi ile aynı olacak bir method aslında.
+        //ctor, clas devrye girdiğinde otomatik çalışıan  bir methodur.
+        //değişik durumlarda initial işlemler yapmak için kullanılır.
+
+
+        public clsTest()
+        {
+            i = 0;
+            s = "Merhaba";
+            b= true;
+        }
+    }
+}

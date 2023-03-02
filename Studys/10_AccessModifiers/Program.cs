0413c62f21ac680716cc25d95ec58bdd67e93356
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-03-02 10:49:05 +0300

    clsTutorials commit edildi

--- Studys/10_AccessModifiers/Program.cs
+++ Studys/10_AccessModifiers/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using _10_AccessModifiers;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        #region public 
+
+
+
+        clsTutorials tutorials = new clsTutorials(); //classtan örnekleme yapıldı nesne oluşturuldu.
+
+        tutorials.setTutorial(1,"csharp eğitimi");
+        Console.WriteLine("Eğitim adı: {0}", tutorials.getTutorialName());
+        
+
+
+
+
+
+
+
+
+
+
+
+        #endregion
+
+        Console.ReadKey();
+    }
+}
---
+++ Studys/10_AccessModifiers/clsTutorials.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_AccessModifiers
+{
+    internal class clsTutorials
+    {
+
+        int TutorialID;
+        string TutorialName;
+
+        public void setTutorial(int pID, string pTutorialName)
+        {
+            this.TutorialID= pID;
+            this.TutorialName= pTutorialName;
+        }
+
+        public  string getTutorialName()
+        {
+            return this.TutorialName;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}

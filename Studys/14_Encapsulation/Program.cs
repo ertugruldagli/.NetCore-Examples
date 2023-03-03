d92c39c235543c63c9bb2d6cb5c81fd5ba2a1414
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-03-03 11:10:32 +0300

    commit the clsBank

---
+++ Studys/14_Encapsulation/Program.cs
@@ -0,0 +1,29 @@
+﻿using _14_Encapsulation;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        #region Normal Durum
+
+        clsBank bank = new clsBank();
+
+        bank.AcountNumber = 123465798;
+        bank.Name = "Santander Bank";
+        bank.Balance = 500;
+
+        bank.GetBalance();
+        bank.WithdrawAccount();
+        bank.depoasit();
+
+        #endregion
+
+
+
+        #region Getter setter
+
+        #endregion
+
+        Console.ReadKey();
+    }
+}
---
+++ Studys/14_Encapsulation/clsBank.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace _14_Encapsulation
+{
+    internal class clsBank
+    {
+        #region Normal Durum
+
+        public long AcountNumber; // Hesap No
+        public string Name;
+        public int Balance; //miktar
+
+        public void GetBalance()
+        {
+
+        }
+
+        public void WithdrawAccount()
+        {
+
+        }
+
+        public void depoasit()
+        {
+
+
+        }
+
+        #endregion
+
+
+        #region Getter setter
+
+        private double balance; // burada dışarıya kapalı bir dğeişken tanımı var
+
+        // set - get methodlarının tanımlanması
+
+        //public getter method: balance değişkeninde tutulan değeri almak için 
+
+        public double GetBalanceCaps()
+        {
+            //burada farklı işlemler var...
+            return this.balance;
+        }
+
+        //public setter method: bu metot balance değişkeninize tutalan değeri set/değiştirmek/güncellemek
+        public void SetBalanceCaps(double balance)
+        {
+           this.balance = balance;// sen benim kendi değişkenimi güncelleyeceksin
+        }
+
+
+
+
+        #endregion
+    }
+}

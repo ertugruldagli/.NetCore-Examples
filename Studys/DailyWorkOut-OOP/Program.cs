4d2adefb7946cb130935f3e8f344fb8e1a5e71c7
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-03-03 14:51:32 +0300

    daily workut bağlı sınıflar

--- Studys/DailyWorkOut-OOP/Program.cs
+++ Studys/DailyWorkOut-OOP/Program.cs
@@ -2,5 +2,19 @@
 
 static void NewMethod()
 {
-    Console.WriteLine("Hello, World!");
+
+    //Human adli bir sınıf oluşturularak ad, soyad, işTanımı, doğumTarihi... ister ekrandan alarak ister  hard coded olacak şekilde
+
+    //tarihsel işlemler için c# dateTime fonksiyonundan/ methodundan yararlanabilrisiniz.
+
+    //fruit isimli bir classınız daha olsun. Burada bir meyve tanımı oluşturunuz... İster ekrandan alarak ister hard coded olacak şekilde
+
+    //Human sınıfından oluşan bir nesneniz elma yesin ve bunu bir mesajla belirtsiniz.
+
+    //Game İsimli bir sınıfınız olsun. Human Sınıfından oluşturulan bir nesneni için istediğiniz bir "....Şuoyunu oynuyor.. " dir mesaj verebilirsiniz.
+
+    //human sınıfından birden fazla nesne üretebilirsiniz..
+
+
+    Console.ReadKey();
 }

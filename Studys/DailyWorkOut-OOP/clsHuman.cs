54f80bd857f999359ba0e002fc71bb7689ac12f3
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-03-03 14:52:16 +0300

    human class commit eidldi

---
+++ Studys/DailyWorkOut-OOP/clsHuman.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyWorkOut_OOP
+{
+    internal class clsHuman
+    {
+    }
+}

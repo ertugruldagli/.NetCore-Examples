4e3609bd66f052ff7da0e42715b36f3a8dae7e8c
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-03-06 12:10:38 +0300

    the end

--- Studys/15AbstractClass/Program.cs
+++ Studys/15AbstractClass/Program.cs
@@ -1,7 +1,35 @@
-﻿internal class Program
+﻿using _15AbstractClass;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+
+    //öncelikle bir beden işçisi oluşturalım 
+
+        clsCreateEmployeeManage  bodyWorker = new clsCreateEmployeeManage();
+
+        bodyWorker.employeeBaseManager = new clsEmployeeManager();
+
+        bodyWorker.Create();
+
+        bodyWorker.getInfo();
+
+        //bir mühendis için 
+
+        clsCreateEmployeeManage engineer=new clsCreateEmployeeManage();
+
+        engineer.employeeBaseManager = new clsEngineerManager();   // mühendisler için
+
+        engineer.Create();
+        engineer.getInfo();
+
+
+
+
+
+
+
+        Console.ReadKey();
     }
 }
--- Studys/15AbstractClass/clsEngineerManager.cs
+++ Studys/15AbstractClass/clsEngineerManager.cs
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace _15AbstractClass
 {
-    internal class clsEngineerManager
+
+    //muhendis sınıfıdan personller için kullanıcakk class
+    internal class clsEngineerManager : clsEmployeeBaseManager
     {
+
+        public clsEngineerManager()
+        {
+            setTitle("muhendis");
+        }
+        public override void doRaise()
+        {
+            double salary = getSalary(); //yani  oluşturulmuş  olan çalışanın o anki maaşını öğreniyorum.
+            double raise = salary + 2500; //raise --> zamlı maaş 
+
+            setSalary(raise); //çalışanın yeni zmalı maaşı işlendi
+        }
+
+        public override void doRaise(double prmRaiseRate)
+        {
+            double salary = getSalary();
+            double raise = salary + (salary * prmRaiseRate) + (salary % 2); // Prim kısmı
+
+            setSalary(raise);  //yeni güncellenmiş maaş
+        }
     }
 }

d6166cd3c9aaebcf02fe81b5be2bbdcefc1516f9
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-03-06 14:02:40 +0300

    commit the interfaces

---
+++ Studys/16_Interfaces/IDatabase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16_Interfaces
+{
+    internal interface IDatabase
+    {
+
+        //bu interface sadece kodsuz olarak tanımlıyoruz. Yani şablon oluşturuyoruz.
+
+        int login(string userName, string passWord);
+
+        void close();
+
+        void addVaule(string value);
+
+        void deletevalue(string value);
+
+        void updatevalue(string value);
+
+        void getValue();
+        
+    }
+}
---
+++ Studys/16_Interfaces/clsOracleDbManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16_Interfaces
+{
+    internal class clsOracleDbManager : IDatabase
+    {
+
+        private string value;
+        private string userName="oracle";
+        private string password="admin";
+        public void addVaule(string value)
+        {
+            this.value = value;
+        }
+
+        public void close()
+        {
+            Console.WriteLine("Oracle DB bağlantısı başarılı bir şekilde sonlandırıldı.");
+        }
+
+        public void deletevalue(string value)
+        {
+           this.value=null;
+
+            Console.WriteLine("Veri tabanı üzerindeki bilgiler silindi!");
+        }
+
+        public void getValue()
+        {
+
+        }
+
+        public int login(string userName, string passWord)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void updatevalue(string value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

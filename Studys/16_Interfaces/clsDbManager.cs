8562ac98fb3ed0087f2612315cf25d89fc1ccd39
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-03-06 14:49:33 +0300

    same changes

---
+++ Studys/16_Interfaces/clsDbManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16_Interfaces
+{
+    internal class clsDbManager
+    {
+
+        private IDatabase database;
+
+        public clsDbManager() { }
+
+        public clsDbManager(IDatabase database) { 
+            
+            this.database = database;
+        }
+
+        public void loginDB()
+        {
+            string userName, passWord;
+
+            Console.WriteLine("Veritabanı bağlantısı  \n\n kullanici ad:");
+            userName= Console.ReadLine();
+
+            Console.WriteLine("Veritabanı bağlantısı  \n\n kullanici password:");
+            passWord = Console.ReadLine();
+
+            if (database.login(userName,passWord)==1)
+            {
+               
+            }
+        }
+    }
+}

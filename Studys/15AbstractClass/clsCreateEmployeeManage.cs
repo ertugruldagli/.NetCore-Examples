1ba3e067ec9447827c9cd1f3c738b2d8b27435d9
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-03-06 11:55:25 +0300

    commit the engineer class

---
+++ Studys/15AbstractClass/clsCreateEmployeeManage.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15AbstractClass
+{
+
+    //burası elemanı yoktan var edecek... Personel listesi oluşturma gibi
+
+    internal class clsCreateEmployeeManage
+    {
+        public clsEmployeeBaseManager employeeBaseManager;
+        private string name, surName;
+        private double salary; 
+
+        public void Create()
+        {
+            Console.WriteLine("<Create Employee Manager>\n\n -> Çalışanın ismi: ");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Çalışanın soyismi: ");
+            surName = Console.ReadLine();
+
+            Console.WriteLine("-> Çalışanın Maaşı: ");
+            salary = Convert.ToInt32(Console.ReadLine());
+
+
+            employeeBaseManager.setInfo(name,surName,salary);
+        }
+
+        public void getInfo()
+        {
+            employeeBaseManager.getInfo();
+        }
+
+        public void doRaise()
+        {
+            employeeBaseManager.doRaise();//Normal çalışan için zam methodunu çağırıyoruz. 
+            Console.WriteLine($"Zam uygulandı \n\n Yeni maaş: {employeeBaseManager.getSalary} PNG kina \n\n" );
+        }
+
+        public void doRaise(double pRaiseRate)
+        {
+            employeeBaseManager.doRaise(pRaiseRate);
+
+          
+            Console.WriteLine($"primli Zam uygulandı \n\n Yeni maaş: {employeeBaseManager.getSalary} PNG kina \n\n");
+        }
+    }
+}
---
+++ Studys/15AbstractClass/clsEmployeeManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15AbstractClass
+{
+    internal class clsEmployeeManager : clsEmployeeBaseManager
+    {
+
+        
+
+        public clsEmployeeManager() //bu class ilk oluştuğunda default en taban title tanımını oluşturmak için 
+        {
+            setTitle("Beden İşçisi");
+        }
+
+
+
+        public override void doRaise() // çalışana zam yapan method 
+        {
+            double salary = getSalary(); //yani  oluşturulmuş  olan çalışanın o anki maaşını öğreniyorum.
+            double raise = salary + 500; //raise --> zamlı maaş 
+
+            setSalary(raise); //çalışanın yeni zmalı maaşı işlendi
+
+        }
+
+        public override void doRaise(double prmRaiseRate) // çalışana zam yapan (+prim) methot 
+        {
+            double salary = getSalary();
+            double raise = salary + (salary*prmRaiseRate);
+
+            setSalary(raise);  //yeni güncellenmiş maaş
+        }
+    }
+}

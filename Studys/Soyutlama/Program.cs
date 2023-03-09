e698b1429bd2cfb861655f4f6bc3a561d160143a
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-03-09 22:09:37 +0300

    commit the all

---
+++ Studys/Soyutlama/Program.cs
@@ -0,0 +1,33 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Database database = new Oracle();
+        database.remove();
+        database.add();
+    }
+
+    abstract class Database
+    {
+        public void add()
+        {
+            Console.WriteLine("added");
+        }
+        public abstract void remove();
+    }
+    class SqlServer:Database { 
+        public override void remove()
+        {
+            Console.WriteLine("silindi");
+        }
+    
+    }
+    class Oracle : Database
+    {
+        public override void remove()
+        {
+            Console.WriteLine("oracledan silindi");
+        }
+    }
+
+}

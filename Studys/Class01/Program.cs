259a667007560b4387a69f42bcff68385bf03d65
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-03-09 21:48:51 +0300

    same changes

--- Studys/Class01/Program.cs
+++ Studys/Class01/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        Person[] persons = new Person[3]
+        {
+            new Customers
+            { Name="engin"},
+            new Student
+            { bolum="mat101"},
+            new Person
+            {Id=12}
+        };
+        foreach (var person in persons)
+        {
+            Console.WriteLine(person.Id + person.Name );
+        }
+        Console.ReadKey();
+    }
+    class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+    }
+
+    class Customers: Person
+    {
+        public string city { get; set; }
+    }
+    class Student : Person
+    {
+        public string bolum { get; set; }
+    }
+}

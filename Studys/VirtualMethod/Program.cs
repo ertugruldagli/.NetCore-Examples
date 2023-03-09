255e4b2e8716f688a72eeff502d26f0143d1854f
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-03-09 22:00:16 +0300

    same changes

---
+++ Studys/VirtualMethod/Program.cs
@@ -0,0 +1,38 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        SqlServer s=new SqlServer();
+        s.add();
+
+        MySql m=new MySql();
+        m.add();
+
+
+        Console.ReadLine();
+    }
+
+    class Database
+    {
+        public virtual void add()
+        {
+            Console.WriteLine("add");
+        }
+        public virtual void remove() {
+            Console.WriteLine("remove");
+                }
+
+
+    }
+    class SqlServer: Database { 
+        public override void add()
+        {
+            Console.WriteLine("sqlden");
+        }
+    }
+    class MySql: Database {
+    
+    
+    }
+
+}

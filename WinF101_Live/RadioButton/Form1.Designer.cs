c00835d4f9eb3c12cc2fcb41283fa6e17bee5a95
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-03-16 14:33:05 +0300

    commit

---
+++ WinF101_Live/RadioButton/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace RadioButton
+{
+    partial class frmRadioButon
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rbtnAzalt = new System.Windows.Forms.RadioButton();
+            this.rbtnArtır = new System.Windows.Forms.RadioButton();
+            this.lblSonuc = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnIslem = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rbtnAzalt
+            // 
+            this.rbtnAzalt.AutoSize = true;
+            this.rbtnAzalt.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rbtnAzalt.Location = new System.Drawing.Point(22, 24);
+            this.rbtnAzalt.Name = "rbtnAzalt";
+            this.rbtnAzalt.Size = new System.Drawing.Size(93, 35);
+            this.rbtnAzalt.TabIndex = 0;
+            this.rbtnAzalt.TabStop = true;
+            this.rbtnAzalt.Text = "Azalt";
+            this.rbtnAzalt.UseVisualStyleBackColor = true;
+            // 
+            // rbtnArtır
+            // 
+            this.rbtnArtır.AutoSize = true;
+            this.rbtnArtır.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rbtnArtır.Location = new System.Drawing.Point(22, 61);
+            this.rbtnArtır.Name = "rbtnArtır";
+            this.rbtnArtır.Size = new System.Drawing.Size(82, 35);
+            this.rbtnArtır.TabIndex = 1;
+            this.rbtnArtır.TabStop = true;
+            this.rbtnArtır.Text = "Artır";
+            this.rbtnArtır.UseVisualStyleBackColor = true;
+            // 
+            // lblSonuc
+            // 
+            this.lblSonuc.AutoSize = true;
+            this.lblSonuc.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSonuc.Location = new System.Drawing.Point(77, 120);
+            this.lblSonuc.Name = "lblSonuc";
+            this.lblSonuc.Size = new System.Drawing.Size(64, 25);
+            this.lblSonuc.TabIndex = 2;
+            this.lblSonuc.Text = "label1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 129);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Sonuc";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.numericUpDown1.Location = new System.Drawing.Point(207, 58);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(82, 38);
+            this.numericUpDown1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(201, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 31);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Miktar";
+            // 
+            // btnIslem
+            // 
+            this.btnIslem.Location = new System.Drawing.Point(198, 102);
+            this.btnIslem.Name = "btnIslem";
+            this.btnIslem.Size = new System.Drawing.Size(100, 27);
+            this.btnIslem.TabIndex = 6;
+            this.btnIslem.Text = "İslem";
+            this.btnIslem.UseVisualStyleBackColor = true;
+            // 
+            // frmRadioButon
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(310, 199);
+            this.Controls.Add(this.btnIslem);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblSonuc);
+            this.Controls.Add(this.rbtnArtır);
+            this.Controls.Add(this.rbtnAzalt);
+            this.Name = "frmRadioButon";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton rbtnAzalt;
+        private System.Windows.Forms.RadioButton rbtnArtır;
+        private System.Windows.Forms.Label lblSonuc;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnIslem;
+    }
+}
+

20a910bda30ba8e8543e2ee952ea19c7cb9730ae
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-03-16 09:10:20 +0300

    same changces

--- Studys/AdoNetDemo/ProductDal.cs
+++ Studys/AdoNetDemo/ProductDal.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,9 @@ namespace AdoNetDemo
 {
     internal class ProductDal
     {
+        public List<Products> GetAll()
+        {
+            SqlConnection conn = new SqlConnection();
+        }
     }
 }

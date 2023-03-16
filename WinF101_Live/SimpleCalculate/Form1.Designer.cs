9277691216e1a23154c70abe1b57d3f1b497d991
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-03-16 11:19:54 +0300

    same chancegs

--- WinF101_Live/SimpleCalculate/Form1.Designer.cs
+++ WinF101_Live/SimpleCalculate/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace SimpleCalculate
 {
-    partial class Form1
+    partial class frmSimple
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,105 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnToplam = new System.Windows.Forms.Button();
+            this.tboxSayi1 = new System.Windows.Forms.TextBox();
+            this.tboxSayi2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnToplam
+            // 
+            this.btnToplam.Location = new System.Drawing.Point(101, 129);
+            this.btnToplam.Name = "btnToplam";
+            this.btnToplam.Size = new System.Drawing.Size(109, 25);
+            this.btnToplam.TabIndex = 0;
+            this.btnToplam.Text = "Hesapla";
+            this.btnToplam.UseVisualStyleBackColor = true;
+            this.btnToplam.Click += new System.EventHandler(this.btnToplam_Click);
+            // 
+            // tboxSayi1
+            // 
+            this.tboxSayi1.Location = new System.Drawing.Point(127, 64);
+            this.tboxSayi1.Name = "tboxSayi1";
+            this.tboxSayi1.Size = new System.Drawing.Size(60, 20);
+            this.tboxSayi1.TabIndex = 1;
+            this.tboxSayi1.Enter += new System.EventHandler(this.tboxSayi1_Enter);
+            // 
+            // tboxSayi2
+            // 
+            this.tboxSayi2.Location = new System.Drawing.Point(127, 90);
+            this.tboxSayi2.Name = "tboxSayi2";
+            this.tboxSayi2.Size = new System.Drawing.Size(60, 20);
+            this.tboxSayi2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Sayi 1 Griniz:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(42, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Sayi 2 Griniz:";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExit.Location = new System.Drawing.Point(275, 198);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(63, 25);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // frmSimple
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(350, 235);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tboxSayi2);
+            this.Controls.Add(this.tboxSayi1);
+            this.Controls.Add(this.btnToplam);
+            this.Name = "frmSimple";
+            this.Text = "SimpleCalculate";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnToplam;
+        private System.Windows.Forms.TextBox tboxSayi1;
+        private System.Windows.Forms.TextBox tboxSayi2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnExit;
     }
 }
 
--- WinF101_Live/SimpleCalculate/Form1.cs
+++ WinF101_Live/SimpleCalculate/Form1.cs
@@ -10,11 +10,41 @@ using System.Windows.Forms;
 
 namespace SimpleCalculate
 {
-    public partial class Form1 : Form
+    public partial class frmSimple : Form
     {
-        public Form1()
+        public frmSimple()
         {
             InitializeComponent();
+        }
+
+        private void btnToplam_Click(object sender, EventArgs e)
+        {
+            int sayi1, sayi2, toplam = 0;
+               
+
+            sayi1 = int.Parse(tboxSayi1.Text);
+            sayi2= int.Parse(tboxSayi2.Text);
+
+            toplam = sayi1+sayi2;
+
+            //MessageBox.Show(Convert.ToString(toplam));
+
+            MessageBox.Show("İşlemin Sonucu: " + toplam.ToString());
+
+            tboxSayi1.Clear();
+            tboxSayi2.Clear();
+
+            tboxSayi1.Focus();// imleci textboxun üstüne konumlandır.
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void tboxSayi1_Enter(object sender, EventArgs e)
+        {
+            MessageBox.Show("şu an bu componenteyim");
         }
     }
 }

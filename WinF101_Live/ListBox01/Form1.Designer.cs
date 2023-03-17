e1e8b407013524a02743611c09be4e11a9f97669
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-03-17 09:59:16 +0300

    same changes

---
+++ WinF101_Live/ListBox01/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace ListBox01
+{
+    partial class frmListBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtAdet = new System.Windows.Forms.TextBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnSayilar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.liBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(216, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Adet: ";
+            // 
+            // txtAdet
+            // 
+            this.txtAdet.Location = new System.Drawing.Point(257, 46);
+            this.txtAdet.Name = "txtAdet";
+            this.txtAdet.Size = new System.Drawing.Size(110, 20);
+            this.txtAdet.TabIndex = 10;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExit.Location = new System.Drawing.Point(336, 276);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(52, 29);
+            this.btnExit.TabIndex = 9;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnSayilar
+            // 
+            this.btnSayilar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSayilar.Location = new System.Drawing.Point(247, 76);
+            this.btnSayilar.Name = "btnSayilar";
+            this.btnSayilar.Size = new System.Drawing.Size(132, 33);
+            this.btnSayilar.TabIndex = 8;
+            this.btnSayilar.Text = "Liste Oluştur";
+            this.btnSayilar.UseVisualStyleBackColor = true;
+            this.btnSayilar.Click += new System.EventHandler(this.btnSayilar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(29, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 25);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Liste:";
+            // 
+            // liBox
+            // 
+            this.liBox.FormattingEnabled = true;
+            this.liBox.Location = new System.Drawing.Point(34, 104);
+            this.liBox.Name = "liBox";
+            this.liBox.Size = new System.Drawing.Size(133, 108);
+            this.liBox.TabIndex = 12;
+            // 
+            // frmListBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(400, 317);
+            this.Controls.Add(this.liBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtAdet);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnSayilar);
+            this.Controls.Add(this.label1);
+            this.Name = "frmListBox";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmListBox";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtAdet;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnSayilar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox liBox;
+    }
+}
+
---
+++ WinF101_Live/ListBox01/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListBox01
+{
+    public partial class frmListBox : Form
+    {
+        public frmListBox()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSayilar_Click(object sender, EventArgs e)
+        {
+            liBox.Items.Clear();
+
+            int adet = int.Parse(txtAdet.Text);
+            for (int i = 1; i <= adet; i++)
+            {
+                string sayiText = i.ToString();
+                liBox.Items.Add(sayiText + ". sayi ");
+
+            }
+            
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}

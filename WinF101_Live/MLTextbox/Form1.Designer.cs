540ac4e11dd8c1b7b056ef09469e43e664be5886
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-03-17 12:34:45 +0300

    the end

---
+++ WinF101_Live/MLTextbox/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace MLTextbox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tBoxML = new System.Windows.Forms.TextBox();
+            this.btnGetir = new System.Windows.Forms.Button();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
+            this.SuspendLayout();
+            // 
+            // tBoxML
+            // 
+            this.tBoxML.Location = new System.Drawing.Point(34, 42);
+            this.tBoxML.Multiline = true;
+            this.tBoxML.Name = "tBoxML";
+            this.tBoxML.Size = new System.Drawing.Size(115, 68);
+            this.tBoxML.TabIndex = 0;
+            // 
+            // btnGetir
+            // 
+            this.btnGetir.Location = new System.Drawing.Point(59, 116);
+            this.btnGetir.Name = "btnGetir";
+            this.btnGetir.Size = new System.Drawing.Size(60, 23);
+            this.btnGetir.TabIndex = 1;
+            this.btnGetir.Text = "Getir";
+            this.btnGetir.UseVisualStyleBackColor = true;
+            this.btnGetir.Click += new System.EventHandler(this.btnGetir_Click);
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Location = new System.Drawing.Point(72, 184);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(0, 13);
+            this.lblMessage.TabIndex = 2;
+            this.lblMessage.Visible = false;
+            // 
+            // vScrollBar1
+            // 
+            this.vScrollBar1.Location = new System.Drawing.Point(152, 42);
+            this.vScrollBar1.Name = "vScrollBar1";
+            this.vScrollBar1.Size = new System.Drawing.Size(17, 80);
+            this.vScrollBar1.TabIndex = 3;
+            this.vScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.vScrollBar1_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(327, 355);
+            this.Controls.Add(this.vScrollBar1);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.btnGetir);
+            this.Controls.Add(this.tBoxML);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tBoxML;
+        private System.Windows.Forms.Button btnGetir;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.VScrollBar vScrollBar1;
+    }
+}
+
---
+++ WinF101_Live/MLTextbox/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MLTextbox
+{
+    public partial class Form1 : Form
+    {
+        Random rnd = new Random();
+
+        int rsTam;
+        double rsOndalik;
+        double gercekSayi;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGetir_Click(object sender, EventArgs e)
+        {
+      
+            IslemYap();
+            MesajVer("merhaba");
+            SayiUret();
+        }
+
+        public void IslemYap()
+        {
+            
+                tBoxML.Text += (SayiUret().ToString()) + "\n\n\n";
+            
+        }
+
+        public void MesajVer(string prmMessage)
+        {
+            lblMessage.Visible = true;
+            // lblMessage.Text = "this is my World!";
+            lblMessage.Text += prmMessage;
+           
+        }
+
+        public double SayiUret()
+        {
+           rsTam=rnd.Next(1,10);
+           rsOndalik = rnd.NextDouble(); //nextDouble methodu sadece 0 ile 1 arasında değer üretir.
+
+           return gercekSayi=rsTam+rsOndalik;
+        }
+
+        private void vScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            
+        }
+    }
+}

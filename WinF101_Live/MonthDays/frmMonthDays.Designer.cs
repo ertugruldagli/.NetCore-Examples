4823fbc6dcb6fa3a5fb015336f155744dd38c152
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-03-20 14:27:45 +0300

    some changes

---
+++ WinF101_Live/MonthDays/frmMonthDays.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace MonthDays
+{
+    partial class frmMonthDays
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nudYil = new System.Windows.Forms.NumericUpDown();
+            this.cBoxAy = new System.Windows.Forms.ComboBox();
+            this.btnListele = new System.Windows.Forms.Button();
+            this.liBoxAylar = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.nudYil)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnExit
+            // 
+            this.btnExit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExit.Location = new System.Drawing.Point(258, 222);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(74, 20);
+            this.btnExit.TabIndex = 0;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(32, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 14);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Yıl: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(33, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 14);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ay: ";
+            // 
+            // nudYil
+            // 
+            this.nudYil.Location = new System.Drawing.Point(66, 65);
+            this.nudYil.Maximum = new decimal(new int[] {
+            2024,
+            0,
+            0,
+            0});
+            this.nudYil.Name = "nudYil";
+            this.nudYil.Size = new System.Drawing.Size(105, 20);
+            this.nudYil.TabIndex = 3;
+            // 
+            // cBoxAy
+            // 
+            this.cBoxAy.FormattingEnabled = true;
+            this.cBoxAy.Location = new System.Drawing.Point(66, 95);
+            this.cBoxAy.Name = "cBoxAy";
+            this.cBoxAy.Size = new System.Drawing.Size(105, 21);
+            this.cBoxAy.TabIndex = 4;
+            // 
+            // btnListele
+            // 
+            this.btnListele.Location = new System.Drawing.Point(78, 122);
+            this.btnListele.Name = "btnListele";
+            this.btnListele.Size = new System.Drawing.Size(77, 25);
+            this.btnListele.TabIndex = 5;
+            this.btnListele.Text = "Listele";
+            this.btnListele.UseVisualStyleBackColor = true;
+            this.btnListele.Click += new System.EventHandler(this.btnListele_Click);
+            // 
+            // liBoxAylar
+            // 
+            this.liBoxAylar.FormattingEnabled = true;
+            this.liBoxAylar.Location = new System.Drawing.Point(202, 33);
+            this.liBoxAylar.Name = "liBoxAylar";
+            this.liBoxAylar.Size = new System.Drawing.Size(121, 134);
+            this.liBoxAylar.TabIndex = 6;
+            // 
+            // frmMonthDays
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(344, 254);
+            this.Controls.Add(this.liBoxAylar);
+            this.Controls.Add(this.btnListele);
+            this.Controls.Add(this.cBoxAy);
+            this.Controls.Add(this.nudYil);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExit);
+            this.Name = "frmMonthDays";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MonthDays";
+            this.Load += new System.EventHandler(this.frmMonthDays_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nudYil)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown nudYil;
+        private System.Windows.Forms.ComboBox cBoxAy;
+        private System.Windows.Forms.Button btnListele;
+        private System.Windows.Forms.ListBox liBoxAylar;
+    }
+}
+
---
+++ WinF101_Live/MonthDays/frmMonthDays.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MonthDays
+{
+    public partial class frmMonthDays : Form
+    {
+
+        DateTimeFormatInfo formatTR = CultureInfo.GetCultureInfo("tr-TR").DateTimeFormat;// Yani bu değişkene türkçe tarih formatı atandı..
+
+        public frmMonthDays()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void frmMonthDays_Load(object sender, EventArgs e)
+        {
+            //Form ekranına gelirken 
+            // 1. nudYil kontrolünün içindeki gösterilecek bilgiyi günümüzün yılı olarak göstersin
+            //2. cboxAy isimli comboboxın otomatik olarak ay adlarıyla doldurulsun ... ay adlarıyla değiştirilsin
+
+            int yil = Convert.ToInt32(DateTime.Now.Year); // default tarihin yıl bilgisini getirir.(şimdiki yılı baz alır.) 
+
+            nudYil.Value= yil;
+
+            for (int i =0; i <12; i++)
+            {
+                cBoxAy.Items.Add(formatTR.MonthNames[i]); 
+            }
+
+
+        }
+
+        private void btnListele_Click(object sender, EventArgs e)
+        {
+            liBoxAylar.Items.Clear();
+
+            int yil= Convert.ToInt32(nudYil.Value);
+
+            int ay = cBoxAy.SelectedIndex + 1;
+
+            //nud dan ve cboxtan seçilen opsiyonlara göre benim bir tarih  bilgisi olarak programa söylemem lazım
+
+            DateTime tarih = new DateTime(yil,ay,1);
+
+            //seçilen ayda kaç gün var onu öğrenmem lazım
+
+            int totalDaysCount = DateTime.DaysInMonth(yil,ay);
+
+            for (int i = 1; i < totalDaysCount; i++)
+            {
+                liBoxAylar.Items.Add(tarih.Day + " " + cBoxAy.Text + " " + formatTR.DayNames[(int)tarih.DayOfWeek]);
+
+                tarih=tarih.AddDays(i);
+            }
+        }
+    }
+}

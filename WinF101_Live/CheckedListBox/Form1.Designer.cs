303bb12d27f83c776b7410ab576f8322e3a33374
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-03-20 11:50:33 +0300

    the commit

--- WinF101_Live/CheckedListBox/Form1.Designer.cs
+++ WinF101_Live/CheckedListBox/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace CheckedListBox
 {
-    partial class Form1
+    partial class frmCheckedListBox
     {
         /// <summary>
         /// Required designer variable.
@@ -28,20 +28,81 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnExit = new System.Windows.Forms.Button();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnAktar = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // Form1
+            // btnExit
+            // 
+            this.btnExit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExit.Location = new System.Drawing.Point(411, 315);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(52, 30);
+            this.btnExit.TabIndex = 0;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "Volvo XC70",
+            "Volkswagen Beetle(1974)",
+            "Ford Bronco",
+            "Bugatti Veyron",
+            "Mercedes 234",
+            "Renault Altair"});
+            this.checkedListBox1.Location = new System.Drawing.Point(12, 78);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(181, 124);
+            this.checkedListBox1.TabIndex = 1;
+            this.checkedListBox1.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.checkedListBox1_ItemCheck);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(285, 78);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(181, 134);
+            this.listBox1.TabIndex = 2;
+            // 
+            // btnAktar
+            // 
+            this.btnAktar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAktar.Location = new System.Drawing.Point(213, 126);
+            this.btnAktar.Name = "btnAktar";
+            this.btnAktar.Size = new System.Drawing.Size(52, 30);
+            this.btnAktar.TabIndex = 3;
+            this.btnAktar.Text = "Aktar";
+            this.btnAktar.UseVisualStyleBackColor = true;
+            this.btnAktar.Click += new System.EventHandler(this.btnAktar_Click);
+            // 
+            // frmCheckedListBox
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(389, 443);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.DodgerBlue;
+            this.ClientSize = new System.Drawing.Size(478, 355);
+            this.Controls.Add(this.btnAktar);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.btnExit);
+            this.Name = "frmCheckedListBox";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Checked List Box";
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnAktar;
     }
 }
 
--- WinF101_Live/CheckedListBox/Form1.cs
+++ WinF101_Live/CheckedListBox/Form1.cs
@@ -10,11 +10,59 @@ using System.Windows.Forms;
 
 namespace CheckedListBox
 {
-    public partial class Form1 : Form
+    public partial class frmCheckedListBox : Form
     {
-        public Form1()
+        public frmCheckedListBox()
         {
             InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void checkedListBox1_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            int checkedOptionIndex; //Tıklanılan ve check haline gelen seçeneği seçin
+
+            string checkedContentText; // içeriğindeki text bilgi
+
+            //seçilen indeksin elemanın indeksini öğrenmem lazım
+            checkedOptionIndex = e.Index;
+
+            // içindeki yazan texti öğreneyim
+            checkedContentText = checkedListBox1.Items[checkedOptionIndex].ToString();
+
+            if (listBox1.Items.IndexOf(checkedContentText) > -1)
+                return;//-1 burada araştırılan textin olduğunu gösterir. Bu yüzden hiçbir işlem yapmadan methoddan çık.
+
+            //eğer listboxsında seçili değer yoksa demek ki ekleyeceğim ama 
+            //checked listboxsın çalışma prensibine göre acaba seçtiğim satırın checkini işaretledi mi kontrolünü yap
+
+            bool contentChecked = checkedListBox1.GetItemChecked(checkedOptionIndex);
+            //yukarıdaki açıklama için bir lojik değişkenle bunu anlatacağım
+            // üzerinde durulan satırı seçip/seçilmediği anlaşılacak
+
+            if (contentChecked==false)
+            {
+                listBox1.Items.Add(checkedContentText);
+                
+            }
+            else
+            {
+                listBox1.Items.RemoveAt(checkedOptionIndex);
+            }
+        }
+
+        private void btnAktar_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+            for (int i = 0; i < checkedListBox1.CheckedItems.Count; i++)
+            {
+                listBox1.Items.Add(checkedListBox1.CheckedItems[i].ToString());
+            }
         }
     }
 }

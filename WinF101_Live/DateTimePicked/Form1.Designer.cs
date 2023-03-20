7976a2aa12b9c0dc602e0c27000f8cb50f2b7499
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-03-20 12:21:32 +0300

    the end

---
+++ WinF101_Live/DateTimePicked/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace DateTimePicked
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtpStart = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dtpFinish = new System.Windows.Forms.DateTimePicker();
+            this.liBoxResult = new System.Windows.Forms.ListBox();
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // dtpStart
+            // 
+            this.dtpStart.Location = new System.Drawing.Point(133, 49);
+            this.dtpStart.Name = "dtpStart";
+            this.dtpStart.Size = new System.Drawing.Size(154, 20);
+            this.dtpStart.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Başlangıç Tarihi";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Bitiş Tarihi";
+            // 
+            // dtpFinish
+            // 
+            this.dtpFinish.Location = new System.Drawing.Point(133, 84);
+            this.dtpFinish.Name = "dtpFinish";
+            this.dtpFinish.Size = new System.Drawing.Size(154, 20);
+            this.dtpFinish.TabIndex = 2;
+            // 
+            // liBoxResult
+            // 
+            this.liBoxResult.FormattingEnabled = true;
+            this.liBoxResult.Location = new System.Drawing.Point(108, 167);
+            this.liBoxResult.Name = "liBoxResult";
+            this.liBoxResult.Size = new System.Drawing.Size(201, 95);
+            this.liBoxResult.TabIndex = 5;
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.Location = new System.Drawing.Point(166, 120);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(77, 28);
+            this.btnHesapla.TabIndex = 4;
+            this.btnHesapla.Text = "hesapla";
+            this.btnHesapla.UseVisualStyleBackColor = true;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(245, 319);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(77, 28);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 359);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.liBoxResult);
+            this.Controls.Add(this.btnHesapla);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dtpFinish);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dtpStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dtpStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dtpFinish;
+        private System.Windows.Forms.ListBox liBoxResult;
+        private System.Windows.Forms.Button btnHesapla;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ WinF101_Live/DateTimePicked/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DateTimePicked
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            DateTime startDate=Convert.ToDateTime(dtpStart.Text);
+
+            DateTime endDate = Convert.ToDateTime(dtpFinish.Text);
+
+            TimeSpan sonuc=endDate.Subtract(startDate); // iki tarih arsındaki farkı alıyor
+
+            string year = Convert.ToString(Math.Round(sonuc.TotalDays/365,4)) + " Yıl veya";
+            string Month = Convert.ToString(Math.Round(sonuc.TotalDays / 30, 2)) + " Ay veya";
+            string day = Convert.ToString(Math.Round(sonuc.TotalDays, 2)) + " Gun veya";
+            string hour = Convert.ToString(Math.Round(sonuc.TotalHours, 2)) + " Saat veya";
+            string Minute = Convert.ToString(Math.Round(sonuc.TotalMinutes, 2)) +  " Dakika ";
+
+            liBoxResult.Items.Clear();
+            liBoxResult.Items.Add("--------------------------");
+            liBoxResult.Items.Add(year);
+            liBoxResult.Items.Add(Month);
+            liBoxResult.Items.Add(day);
+            liBoxResult.Items.Add(hour);
+            liBoxResult.Items.Add(Minute);
+            
+           
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}

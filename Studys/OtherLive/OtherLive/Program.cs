b097bebe3e8f982b6e6e7140faa50204999ae756
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-04-17 11:22:09 +0300

    some changes

--- Studys/OtherLive/OtherLive/Program.cs
+++ Studys/OtherLive/OtherLive/Program.cs
@@ -5,9 +5,20 @@
         //Generic yöntemini kullanarak.... 
 
         Example<string> Example_1=new Example<string>();
-
-        Example_1.example_1 = "ERtugrul Daglı";
+        Example_1.example_1 = "Ertugrul Daglı";
         Example_1.example_2("c#");
+
+
+        Example<int> Example_2 = new Example<int>();
+        Example_2.example_1 = 20;
+        Example_2.example_2(200);
+
+
+        Example<double> Example_3 = new Example<double>();
+        Example_3.example_1 = 132.55;
+        Example_3.example_2(56.23);
+
+
 
 
 
@@ -21,7 +32,7 @@
 
         public void examaple_2(int parameter)
         {
-
+            Console.WriteLine("Parametre değeri: " + parameter);
         }
     
     }

82c44d618d90dde6b042a6066fee5bd760e4d605
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-04-17 11:15:55 +0300

    create the genericClass

--- Studys/OtherLive/OtherLive/Program.cs
+++ Studys/OtherLive/OtherLive/Program.cs
@@ -2,14 +2,40 @@
 {
     private static void Main(string[] args)
     {
-       
+        //Generic yöntemini kullanarak.... 
+
+        Example<string> Example_1=new Example<string>();
+
+        Example_1.example_1 = "ERtugrul Daglı";
+        Example_1.example_2("c#");
+
+
+
     }
 
 
-    public class Example { 
+    public class Example
+    {
+
+        public int expamle_1;
+
+        public void examaple_2(int parameter)
+        {
+
+        }
     
-    
-    
+    }
+
+    //generic harici yapı kullandığında değişkenler, parametreler ve metotların gei dönüş tipleri gibi durumları o sınıftan bir nesne oluştururken belirlenebilmesi
+
+    public class Example<T>
+    {
+        public T example_1;
+
+        public void example_2(T parameter)
+        {
+
+        }
     }
 
 }

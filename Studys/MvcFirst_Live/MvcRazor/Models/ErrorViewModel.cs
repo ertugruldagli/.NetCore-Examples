9bd6615508ee44c12c8aceb1c5dec9da9ada9ea2
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-04-24 14:11:31 +0300

    the commit

---
+++ Studys/MvcFirst_Live/MvcRazor/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MvcRazor.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
